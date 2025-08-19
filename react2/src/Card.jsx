18032d24829c12271f3f4bd17dd29f00f2ac3c38
Aman <22054383@kiit.ac.in>
2025-08-20 00:46:59 +0530

    Added dudh.png image, updated Card.jsx and index.css

--- react2/src/Card.jsx
+++ react2/src/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import profile from './assets/pic.jpg'
+import hlo from './assets/dudh.png'
 
 function Card() {
   return (
     <div className='card'>
-        <img src={profile} alt="Place"/>
+        <img className='im' src={hlo} alt="Place"/>
+        <h2 className='cla'>Dudhu</h2>
+        <p className='ghop'>I have big tits and all of my friends tries to squeez that</p>
     </div>
   )
 }
